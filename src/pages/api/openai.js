ee235b701ea817885582fe9cb88772fdd8b0048d
Tejas Kamtam <tejaskamtam@yahoo.com>
2023-04-15 13:41:29 -0700

    openai fix

--- src/pages/api/openai.js
+++ src/pages/api/openai.js
@@ -1,19 +1,26 @@
-import { Configuration, OpenAIApi } from "openai";
-const configuration = new Configuration({
-    organization: "org-SZLwyfrU6qwtX9czsudt6bBy",
-    apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
 
-// function to generate text from a prompt
-export default async function handler(req, res) {
-    try {
-        const response = await openai.complete({
-            
-        });
+import { Configuration, OpenAIApi } from 'openai';
+export class OpenAI {
+    constructor(apiKey) {
+        // Create the Configuration and OpenAIApi instances
+        this.openai = new OpenAIApi(new Configuration({ apiKey }));
     }
-    catch (err) {
-        console.log(err);
-        res.status(500).json({ error: err });
+    // Asynchronous function to generate text from the OpenAI API
+    async generateText(prompt, model, max_tokens, temperature = 0.85) {
+        try {
+            // Send a request to the OpenAI API to generate text
+            const response = await this.openai.createCompletion({
+                model,
+                prompt,
+                max_tokens,
+                n: 1,
+                temperature,
+            });
+            console.log(`request cost: ${response.data.usage.total_tokens} tokens`);
+            // Return the text of the response
+            return response.data.choices[0].text;
+        } catch (error) {
+            throw error;
+        }
     }
 }
